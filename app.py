b70189fb4acd7e4e05ba216c3cab2b806b060326
caramnix <35668367+caramnix@users.noreply.github.com>
2022-03-07 15:35:53 -0500

    add third viz

--- app.py
+++ app.py
@@ -40,6 +40,7 @@ gender_2012
 
 st.header("Step 2: Visualizations")
 
+
 st.subheader("Vizualization From Perspective 1: Greyscale Heatmap")
 
 heatmap2= alt.Chart(gender_2012).mark_bar().encode(
@@ -85,7 +86,35 @@ st.altair_chart(heatmap+ points + points.transform_regression('diff_M-F_Mortalit
 
 st.markdown("<p style='text-align: center;'>Figure 2: Figure which displays a heatmap of the realtionship between mortality rate and life expectancy; each point corresponds to a country. Axes correspond to the difference between values for men and women. </p>", unsafe_allow_html=True)
 
+
+
+st.subheader("Vizualization From Perspective 3: Inferno Heatmap")
+
+heatmap2= alt.Chart(gender_2012).mark_bar().encode(
+    alt.X('diff_M-F_Mortality_rate:Q', bin=alt.BinParams(maxbins=30), title="Difference in Mortality Rate, M-F"),
+    alt.Y('diff_F-M_Life_expectancy:Q', bin=alt.BinParams(maxbins=30), title="Difference in Life Expectancy, F-M"),
+    alt.Color('count()', scale=alt.Scale(scheme='inferno'))
+).properties(title='Heat Map of Mortality Rate and Life Expectancy (2012)', width=700, height = 700)
+
+points2 = alt.Chart(gender_2012).mark_circle(
+    color='black',
+    size=5,
+).encode(
+    x='diff_M-F_Mortality_rate:Q',
+    y='diff_F-M_Life_expectancy:Q',
+)
+
+#heatmap2 + points2
+
+st.altair_chart(heatmap2+points2, use_container_width = True)
+
+st.markdown("<p style='text-align: center;'>Figure 3: Figure which displays a heatmap of the realtionship between mortality rate and life expectancy; each point corresponds to a country. Positive values indicate better conditions for women on both axes. Data from the World Bank, 2012. </p>", unsafe_allow_html=True)
+
+
+
 st.subheader("Discussion")
-st.write("Visualization from perspective 1 is better than the visualization from perspective 2. First, the greyscale heat map is much easier to interpret than the rainbow heat map. Rainbow colors are inherently unordered and work best for categorical data, rather than quantitative data, which is what we are working with here. It is much easier to interpret that the darker the color (black) the more observations than it is to first check the color mapping and realize that dark blue was assigned to be the color for the highest concentration of points.")
+st.write("Visualization from perspective 1 is better than the visualization from perspective 2. First, the greyscale heat map is much easier to interpret than the rainbow heat map. Rainbow colors are inherently unordered and work best for categorical data, rather than quantitative data, which is what we are working with here. It is much easier to interpret that the darker the color (black) the more observations than it is to first check the color mapping and realize that green was assigned to be the color for the highest concentration of points.")
 st.write("Second, the axes labels on the perspective 2 visualization are misleading in the sense they make the chart more difficult to interpret. You’ll note that the x-axis label is Difference in Mortality Rate, M-F and the y-axis label is Difference in Life Expectancy, M-F. Which, at first glance seems okay as they are both M-F. However, how we think about life expectancy and mortality are different. Here’s what I mean: on the x axis, positive values indicate that more men died than women in 2012 (which implies a better quality of life for women) and on the y-axis positive values indicate that men had a higher life expectancy than women (indicating worse quality of life for women). Therefore, the visualization from perspective 1, where the axes do have differing M-F is more clearly interpretable in the sense that large positive values on both axes mean good things for women, and large negative values on both indicate things are worse off for women.  So, as the data is primarily in the bottom left corner for visualization 1 we can easily determine that in most countries women have a shorter life expectancy and a higher mortality rate.") 
 st.write("Third, note that the visualization from perspective 2 has an added yellow regression line which is completely unnecessary- it’s chart junk used to distract. Fourth, due to the binning used in the perspective two visualization the figure looks more cluttered and messy than it does in the perspective 1 visualization where data points share boxes, rather than having almost every data point have it’s own box. Lastly, one should note the year is missing from the title in the perspective 2 visualization, so it does not indicate when this relationship occurred temporally. ")
+
+st.write("Visualization from perspective 1 is better than the visualization from perspective 3. I'd argue the greyscale color map is more interpretable than the inferno colormap, as the greyscale color map goes from light to dark (where dark indicates a higher quantity), whereas the inferno color map goes from dark to light (where dark indicates a lower quantity). However, I do think that the inferno colormap is better than the rainbow colormap, as the colors in the inferno colormap are perceptually uniform and do have some sense of dark to light.")
